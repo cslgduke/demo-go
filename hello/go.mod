5e23ac991491765a3de27b33c95e336f3f996c4a
dave <dave.du@sap.com>
2023-04-08 13:07:19 +0800

    create work space

--- hello/go.mod
+++ hello/go.mod
@@ -4,4 +4,13 @@ go 1.20
 
 replace example.com/greetings => ../greetings
 
-require example.com/greetings v0.0.0-00010101000000-000000000000
+require (
+	example.com/greetings v0.0.0-00010101000000-000000000000
+	golang.org/x/example v0.0.0-20220412213650-2e68773dfca0
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.3.3 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,14 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"golang.org/x/example/stringutil"
 	"log"
 )
+import "rsc.io/quote"
 
 func main() {
+	fmt.Println(quote.Go())
+
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 	message, err := greetings.Hello("Dave")
@@ -22,4 +26,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(messages)
+
+	fmt.Println(stringutil.Reverse("Hello"))
 }
