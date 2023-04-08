d15ba456412059e42258bdd87ebb409ba168ef62
dave <dave.du@sap.com>
2023-04-08 10:12:07 +0800

    add unit test

--- hello/hello.go
+++ hello/hello.go
@@ -15,4 +15,11 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+
+	names := []string{"James", "Michal", "Lucas"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
 }
